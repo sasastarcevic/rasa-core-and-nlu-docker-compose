fa9e3166eb3ca4d5a4004de4fc2d1e41a08fd9a4
Gaurav Rawat <gauravbrills14@gmail.com>
2018-10-11 16:56:23 -0400

    use the latest api for starting server

--- run-rasa-core.sh
+++ run-rasa-core.sh
@@ -2,4 +2,4 @@ python -m rasa_nlu.train -c nlu_model_config.json --fixed_model_name current
 
 python -m rasa_core.train -s data/stories.md -d domain.yml -o models/dialogue --epochs 300
 
-python -m rasa_core.server -d models/dialogue -u models/nlu/default/current -o out.log
+python -m rasa_core.run -d models/dialogue -u models/nlu/default/current -o out.log
